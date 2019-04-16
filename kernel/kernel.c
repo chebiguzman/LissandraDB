e1e56593705f46fa90441f76c0564038b362268f
ignacio <ignacio.pugliese@gmail.com>
2019-04-16 13:55:32 -0300

    makefile solo para kernel y dependencias resueltas, el servidor ahora puede recibir ip por parametro opcional

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <commons/log.h>
-//#include "../server.c"
+#include "../server.h"
 char* LOGPATH = "kernel.log";
 //punto de entrada para el programa y el kernel
 int main(int argc, char const *argv[])
@@ -9,5 +9,8 @@ int main(int argc, char const *argv[])
 
     t_log* logger;
     logger = log_create(LOGPATH, "Kernel", 1, LOG_LEVEL_INFO);
+    serverThread(8080,NULL, logger);
+
+
       return 0;
 }
---
+++ server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H_   
+#define SERVER_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> //sock structures
+#include <commons/log.h> //loggger
+#include <commons/string.h> //string append
+#include <unistd.h> //read function
+
+void* serverThread (int portNumber, char * ip, t_log* logger);
+
+#endif
