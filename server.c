2006a455b6166a2d5c3f6780e23cabd639288a8c
ignacio <ignacio.pugliese@gmail.com>
2019-04-16 13:55:32 -0300

    makefile solo para kernel y dependencias resueltas, el servidor ahora puede recibir ip por parametro opcional

--- server.c
+++ server.c
@@ -3,14 +3,15 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <commons/log.h>
-#include <commons/string.h>
+#include <arpa/inet.h> //sock structures
+#include <commons/log.h> //loggger
+#include <commons/string.h> //string append
+#include <unistd.h> //read function
 
 /*
 Esta funcion levanta un servidor por sockets
 portNumber: numero del puerto para el servidor
-ip: ip del servidor
+ip: ip del servidor, si se le pasa null elije cualquiera
 logger: logueador para poder imprimir status de la conexcion
 */
 
@@ -18,7 +19,7 @@ void* serverThread (int portNumber, char * ip, t_log* logger)
 {
 	 int sockfd, newsockfd, clilentNumber;
 	 struct sockaddr_in serverAddress, cli_addr;
-
+     char buffer[512] = {0};
 
     
 	portNumber = htons(portNumber);
@@ -28,7 +29,13 @@ void* serverThread (int portNumber, char * ip, t_log* logger)
     char* log1 = "se setea el servidor a ip: ";
     string_append(&log1, ip);
     log_info(logger, log1);
+
+    //SI se le pasa null a la ip se elije cualquiera disponible
+    if(ip = NULL){
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
+    }
     serverAddress.sin_addr.s_addr =  inet_addr(ip);
+
     serverAddress.sin_port = portNumber;
 
     //argv0 que la coneccion es por medio de sockets, arv1 tipo TCP , argv2 y devuelve un fd (file decriptor)
@@ -44,5 +51,8 @@ void* serverThread (int portNumber, char * ip, t_log* logger)
     clilentNumber = sizeof(cli_addr);
     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilentNumber);
     if(newsockfd<0) exit(1);
+
+    read(newsockfd, buffer, sizeof(buffer));
+    printf("%s", buffer);
 	return 0;
 }
