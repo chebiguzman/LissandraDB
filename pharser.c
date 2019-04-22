28afa97d22cbbea395dc832eea11c9d64c68e279
utnso <utnso@ubuntu-server>
2019-04-22 16:25:43 -0300

    Agrego malloc y accion para cada header.

--- pharser.c
+++ pharser.c
@@ -8,7 +8,7 @@
 
 
 
-typedef struct pakage_select pakage_select;
+typedef struct package_select package_select;
 
 void pharse_bytearray(char* buffer){
     char header[HEADER_BYTE_SIZE];
@@ -16,49 +16,73 @@ void pharse_bytearray(char* buffer){
     printf("header: %s\n", header );
 
     if(!strcmp(header,"SELECT")){
-        pakage_select* pakage = malloc(sizeof(pakage_select));
-        strcpy(pakage->header, header);
+        package_select* package = malloc(sizeof(package_select));
+        strcpy(package->header, header);
         
         int table_name_len = strlen(get_string_from_buffer(buffer, HEADER_BYTE_SIZE))+1; //sumo el \o
-        pakage->table_name = strdup(get_string_from_buffer(buffer, HEADER_BYTE_SIZE));
-        pakage->key = buffer[HEADER_BYTE_SIZE+table_name_len];
+        package->table_name = strdup(get_string_from_buffer(buffer, HEADER_BYTE_SIZE));
+        package->key = buffer[HEADER_BYTE_SIZE+table_name_len];
 
 
-        printf("\n Datos de paquete:\n Header: %s\n Table name: %s\n Key: %d\n", pakage->header, pakage->table_name, buffer[HEADER_BYTE_SIZE+table_name_len]);
-        action_select(pakage);
+        printf("\n Datos de paquete:\n Header: %s\n Table name: %s\n Key: %d\n", package->header, package->table_name, buffer[HEADER_BYTE_SIZE+table_name_len]);
+        action_select(package);
     }
 
     /*
     if(!strcmp(header,"INSERT")){
-        
+        package_select* package = malloc(sizeof(package_insert));
+        strcpy(package->header, header);
+
+        action_insert(package);
     }
 
     if(!strcmp(header,"CREATE")){
-        
+        create_package* package = malloc(sizeof(package_create));
+        strcpy(package->header, header);
+
+        action_create(package);
     }
 
     if(!strcmp(header,"DESCRIBE")){
-        
+        package_describe* package = malloc(sizeof(package_describe));
+        strcpy(package->header, header);
+
+        action_describe(package);
     }
 
     if(!strcmp(header,"DROP")){
-        
+        package_drop* package = malloc(sizeof(package_drop));
+        strcpy(package->header, header);
+
+        action_drop(package);
     }
 
     if(!strcmp(header,"JOURNAL")){
-            
+        package_journal* package = malloc(sizeof(package_journal));
+        strcpy(package->header, header);
+
+        action_journal(package);
     }
 
     if(!strcmp(header,"ADD")){
-            
+        package_add* package = malloc(sizeof(package_add));
+        strcpy(package->header, header);
+
+        action_add(package);
     }
 
     if(!strcmp(header,"RUN")){
-            
+        package_run* package = malloc(sizeof(package_run));
+        strcpy(package->header, header);
+
+        action_run(package);
     }
 
     if(!strcmp(header,"METRICS")){
-            
+        package_metrics* package = malloc(sizeof(package_metrics));
+        strcpy(package->header, header);
+
+        action_metrics(package);
     }*/
 
     
