afcef7d0091c8bc74e1e513e923674a3d43c3820
utnso <utnso@ubuntu-server>
2019-04-22 16:22:54 -0300

    Finalizo las Actions.

--- actions.h
+++ actions.h
@@ -2,6 +2,26 @@
 #define PHARSER_H_
 #include "server.h"
 
-void action_select(pakage_select* select_info);
-void action_insert(pakage_insert* insert_info); //agregar demas interfaces
+void action_select(package_select* select_info);
+
+void action_insert(package_insert* insert_info);
+
+void action_create(package_create* create_info);
+
+void action_describe(package_describe* describe_info);
+
+void action_drop(package_drop* drop_info);
+
+void action_journal(package_journal* journal_info);
+
+void action_add(package_add* add_info);
+
+void action_run(package_run* run_info);
+
+void action_metrics(package_metrics* metrics_info);
+
+
+
+
+
 #endif
