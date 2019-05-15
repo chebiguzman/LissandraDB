9bc0244f45f4017eae239d60a03c8b806e7bd71f
ale <Alejandro.Damian.Bastida@hotmail.com>
2019-05-15 18:22:49 -0300

    teta

--- filesystem/filesystem.c
+++ filesystem/filesystem.c
@@ -10,20 +10,31 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <netdb.h>
-
-//punto de entrada para el programa y el kernel
-int main(int argc, char const *argv[])
-{
-    
-    //set up confg
+//set up confg
     t_config* config = config_create("config");
     char* LOGPATH = config_get_string_value(config, "LOG_PATH");
-    int PORT = config_get_int_value(config, "PORT");
-   
     //set up log
     t_log* logger;
     pthread_t tid;
     logger = log_create(LOGPATH, "Filesystem", 1, LOG_LEVEL_INFO);
+
+//punto de entrada para el programa y el kernel
+int main(int argc, char const *argv[])
+{    
+    
+    typedef struct{
+  char consistency [2];
+  int apartitions;
+  int compactiont;
+    }metadatareg;
+    
+    int PORT = config_get_int_value(config, "PORT");
+   
+   typedef struct{
+   int keey
+   char value [20]
+   }readrow;
+    
 
 
     //set up server
@@ -59,12 +70,53 @@ el return de la funcion es lo que devuelve el fs.
 recibe por parametro un select info definido en server.h*/
 char* action_select(package_select* select_info){
   log_info(logger, "Se recibio una accion select");
-  //aca haces lo que necesites hacer para generar un resultado
-  //ejemplo 
-  char* resultado = obtener_key(select_info->table_name, select_info->key);
-  return resultado; //este resultado es lo que se le manda a memoria / imprime en pantalla
-  /* pero vos no te tenes que preocupar por eso, ya que eso esta resuelto mas arriba en 
-  la implementacion*/
+  char* auxrow;
+  FILE* table=NULL;
+  FILE* metadata=NULL;
+  readrow rowreg;
+  int finded=0;
+  char* ruta=config_get_string_value(config,dir);
+  int total= strlen(ruta)+strlen(select_info->table_name)+13;
+  char* finalmetadata=maloc(total);
+  strcat(finalmetadata,ruta);
+  strcat(finalmetadata,select_info->table_name);
+  strcat(finalmetadata,"metadata.txt");
+  finalmetadata[total]='\0';
+  metadata=fopen(finalmetadata,r+);
+  metadatareg reading;
+  while(!feof(metadata)){
+  fread(reading,sizeof(metadatareg),1,metadata);
+  }
+  fflush(metadata);
+  fclose(metadata);
+  //aca obtendria la posicion del vector donde esta el registro deseado
+  // con ese debo crear la ruta al .bin desado y sabria cual archivo abrir
+  int part= select_info->key % reading.apartitions;
+  char* ruta2= config_get_string_value(config,dir);
+  int totalen= strlen(ruta2)+strlen(select_info->name_table)+1 +strlen((char* select_info->key)+5);
+  char* final=malloc(totalen);
+  strcat(final,ruta2);
+  strcat(final,(char*)part);
+  strcat(final,".bin");
+  final[totalen]='\0';
+  table=fopen(final,r+b);
+ while(finded!=1 && !feof(table)){
+fgets(auxrow,100,table);
+ if(atoi(auxrow)=select_info->key){
+ finded=1;
+  }
+  }
+if(finded!=1){
+  log_info(logger,"no se encontro la row solicitada");
+  return "row no encontrada";
+}
+char* trash= get_string_from_buffer(auxrow,0);
+int index=strlen(trash);
+char* trash2= get_string_from_buffer(auxrow,index+1);
+int index2=strlen(trash2);
+char* row=get_string_from_buffer(auxrow,index+index2+1);
+}
+return row;
 }
 
 void action_insert(package_insert* insert_info){
