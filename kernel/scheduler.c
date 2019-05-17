a406244a4124ac8f23593bafe70c01cac674a695
ignacio <ignacio.pugliese@gmail.com>
2019-05-17 13:52:51 -0300

    multiprocesamiento en proceso

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -3,43 +3,36 @@
 #include "scheduler.h"
 #include<pthread.h>
 
-typedef struct 
-{
-    long quantum;
-    int multi_script_level;
-
-}scheduler_status;
-
-typedef struct 
-{
-    t_queue* scheduler_queue;
-    pthread_mutex_t lock;
-    pthread_cond_t cond;
-}scheduler_queue;
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../pharser.h"
 scheduler_status* status;
 scheduler_queue* queue;
-
-void start_sheduler(){
+t_log* logg;
+void start_sheduler(t_log* log){
+    log_info(log, "se inicia el modulo scheduler");
+    logg = log;
     status= malloc(sizeof(scheduler_status));
     status->multi_script_level = 1;
     status->quantum = 1;
-
-    queue = scheduler_queue_create();
+    
+    queue = malloc(sizeof(scheduler_queue));
+    queue->scheduler_queue = queue_create();
+    int r = pthread_mutex_init(&queue->lock, NULL);
+    pthread_cond_init(&queue->cond,NULL);
     //start service innotifyi
 
+    pthread_t tid;
+    pthread_create(&tid, NULL, exec, NULL);
 
     
 }
 
-scheduler_queue* scheduler_queue_create(){
-    scheduler_queue* q = malloc(sizeof(scheduler_queue));
-    queue_create(queue->scheduler_queue);
-    pthread_mutex_init(&queue->lock, NULL);
-    pthread_cond_init(&queue->cond);
+#include "exec.c"
 
-}
 void lock_queue(){
     pthread_mutex_lock(&queue->lock);
 }
@@ -48,15 +41,18 @@ void unlock_queue(){
 }
 
 
-void schedule(instr_set* instr_set){
+void schedule(t_instr_set* instr_set){
+
     lock_queue();
     queue_push(queue->scheduler_queue, instr_set);
+    log_info(logg, "sh:acrego una instruccion");
     unlock_queue();
+    
     pthread_cond_broadcast(&queue->cond);
-
+    log_info(logg, "llame a exec");
 }
 
-void syscall(char* call){
+void ksyscall(char* call){
 
 }
 
--- kernel/scheduler.h
+++ kernel/scheduler.h
@@ -1,10 +1,20 @@
 #include <commons/collections/queue.h>
-
+#include <pthread.h>
+#include <commons/log.h>
 typedef struct 
 {
     t_queue* instr;
     int doesPrint;
-}instr_set;
+}t_instr_set;
+
+
+
+typedef struct 
+{
+    long quantum;
+    int multi_script_level;
+
+}scheduler_status;
 
 typedef struct 
 {
@@ -12,7 +22,18 @@ typedef struct
     pthread_mutex_t lock;
     pthread_cond_t cond;
 }scheduler_queue;
+typedef struct 
+{
+    t_log* log;
+    scheduler_queue** queue;
+}exec_atrr;
 
-
+void schedule(t_instr_set* instr_set);
+void start_sheduler();
 void lock_queue();
 void unlock_queue();
+void scheduler_queue_create(scheduler_queue** s);
+void schedule(t_instr_set* instr_set);
+void ksyscall(char* call);
+void* exec();
+void updateTasks(t_queue* exec_queue, int exec_size);
