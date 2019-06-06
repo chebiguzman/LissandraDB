4577e796557674ade03fde6393b37e69e384b016
ignacio <ignacio.pugliese@gmail.com>
2019-06-06 03:13:49 -0300

    insert arreglado y terminado

--- filesystem/filesystem.c
+++ filesystem/filesystem.c
@@ -20,43 +20,31 @@
 #include "memtable.h"
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "filesystem.h"
 
 //punto de entrada para el programa y el kernel
 t_log* logger;
 
 //crear memtable
-struct table_node* memtable_p = NULL;
+char* MNT_POINT;
 
 int main(int argc, char const *argv[])
 {
-    
-    typedef struct{
-      char consistency [2];
-      int apartitions;
-      int compactiont;
-    } metadatareg;
-
-    typedef struct{
-      int keey;
-      char value [20];
-    } readrow;
-
+    //las estructuras se van al .h para que quede mas limpio
     //set up confg
     t_config* config = config_create("config");
     char* LOGPATH = config_get_string_value(config, "LOG_PATH");
-    
-    //set up log
-    pthread_t tid;
-    logger = log_create(LOGPATH, "Filesystem", 1, LOG_LEVEL_INFO);
-    
+    MNT_POINT = config_get_string_value(config, "PUNTO_MONTAJE");
     int PORT = config_get_int_value(config, "PORT");
 
+    //set up log
+    logger = log_create(LOGPATH, "Filesystem", 1, LOG_LEVEL_DEBUG);
+      
     //set up server
     server_info* serverInfo = malloc(sizeof(server_info));
-    memset(serverInfo, 0, sizeof( server_info));    
     serverInfo->logger = logger;
     serverInfo->portNumber = PORT;
-
+    pthread_t tid;
     pthread_create(&tid, NULL, create_server, (void*) serverInfo);
 
     //levantar estructura del fileSystem
@@ -64,7 +52,8 @@ int main(int argc, char const *argv[])
     memset(fs_structure_info, 0, sizeof(fs_structure_info));
     fs_structure_info->logger = logger;
     pthread_t tid_fs_structure;
-    pthread_create(&tid_fs_structure, NULL, setup_fs, (void*) fs_structure_info);
+    //lo apague me estass creando los archivos en cualquier lado...
+    //pthread_create(&tid_fs_structure, NULL, setup_fs, (void*) fs_structure_info);
 
     //inicio lectura por consola
     pthread_t tid_console;
@@ -85,17 +74,9 @@ int main(int argc, char const *argv[])
 
 //IMPLEMENTACION DE ACCIONES (Devolver error fuera del subconjunto)
 
-char* obtener_key(char* nombreTabla, int key){
-  //en esta funcion deberias bscar sobre tus archivos y mem table
-  //y devolver ese resultado
-  return string_new();
-}
-
 int tables_count() {
-  t_config* config = config_create("config"); //consultar equipo si tengo que abrir config acá tambien.
   
   //defino el punto de partida para recorrer las tablas
-  char* MNT_POINT = config_get_string_value(config, "PUNTO_MONTAJE");
   char path_table[300];
   path_table[0] = '\0';
   strcat(path_table,MNT_POINT);
@@ -138,10 +119,8 @@ char** tables_names(){
   }
   
   //cargo todos los nombres
-  t_config* config = config_create("config"); //consultar equipo si tengo que abrir config acá tambien.
   
   //defino el punto de partida para recorrer las tablas
-  char* MNT_POINT = config_get_string_value(config, "PUNTO_MONTAJE");
   char path_table[300];
   path_table[0] = '\0';
   strcat(path_table,MNT_POINT);
@@ -181,75 +160,78 @@ char** tables_names(){
   free(names);
 }
 
-/* esta funcion es llamada automaticamnte por el servidor.
-el return de la funcion es lo que devuelve el fs.
-recibe por parametro un select info definido en server.h*/
+
 char* action_select(package_select* select_info){
-  log_info(logger, "Se recibio una accion select");
-  /*char* auxrow;
-  FILE* table=NULL;
-  FILE* metadata=NULL;
-  readrow rowreg;
-  int finded=0;
-  char* ruta=config_get_string_value(config,dir);
-  int total= strlen(ruta)+strlen(select_info->table_name)+13;
-  char* finalmetadata=maloc(total);
-  strcat(finalmetadata,ruta);
-  strcat(finalmetadata,select_info->table_name);
-  strcat(finalmetadata,"metadata.txt");
-  finalmetadata[total]='\0';
-  metadata=fopen(finalmetadata,r+);
-  metadatareg reading;
-  while(!feof(metadata)){
-  fread(reading,sizeof(metadatareg),1,metadata);
+    log_info(logger, "Se recibio una accion select");
+    /*char* auxrow;
+    FILE* table=NULL;
+    FILE* metadata=NULL;
+    readrow rowreg;
+    int finded=0;
+    char* ruta=config_get_string_value(config,dir);
+    int total= strlen(ruta)+strlen(select_info->table_name)+13;
+    char* finalmetadata=maloc(total);
+    strcat(finalmetadata,ruta);
+    strcat(finalmetadata,select_info->table_name);
+    strcat(finalmetadata,"metadata.txt");
+    finalmetadata[total]='\0';
+    metadata=fopen(finalmetadata,r+);
+    metadatareg reading;
+    while(!feof(metadata)){
+    fread(reading,sizeof(metadatareg),1,metadata);
+    }
+    fflush(metadata);
+    fclose(metadata);
+    //aca obtendria la posicion del vector donde esta el registro deseado
+    // con ese debo crear la ruta al .bin desado y sabria cual archivo abrir
+    int part= select_info->key % reading.apartitions;
+    char* ruta2= config_get_string_value(config,dir);
+    int totalen= strlen(ruta2)+strlen(select_info->name_table)+1 +strlen((char* select_info->key)+5);
+    char* final=malloc(totalen);
+    strcat(final,ruta2);
+    strcat(final,(char*)part);
+    strcat(final,".bin");
+    final[totalen]='\0';
+    table=fopen(final,r+b);
+  while(finded!=1 && !feof(table)){
+  fgets(auxrow,100,table);
+  if(atoi(auxrow)=select_info->key){
+  finded=1;
+    }
+    }
+  if(finded!=1){
+    log_info(logger,"no se encontro la row solicitada");
+    return "row no encontrada";
   }
-  fflush(metadata);
-  fclose(metadata);
-  //aca obtendria la posicion del vector donde esta el registro deseado
-  // con ese debo crear la ruta al .bin desado y sabria cual archivo abrir
-  int part= select_info->key % reading.apartitions;
-  char* ruta2= config_get_string_value(config,dir);
-  int totalen= strlen(ruta2)+strlen(select_info->name_table)+1 +strlen((char* select_info->key)+5);
-  char* final=malloc(totalen);
-  strcat(final,ruta2);
-  strcat(final,(char*)part);
-  strcat(final,".bin");
-  final[totalen]='\0';
-  table=fopen(final,r+b);
- while(finded!=1 && !feof(table)){
-fgets(auxrow,100,table);
- if(atoi(auxrow)=select_info->key){
- finded=1;
+  char* trash= get_string_from_buffer(auxrow,0);
+  int index=strlen(trash);
+  char* trash2= get_string_from_buffer(auxrow,index+1);
+  int index2=strlen(trash2);
+  char* row=get_string_from_buffer(auxrow,index+index2+1);
   }
-  }
-if(finded!=1){
-  log_info(logger,"no se encontro la row solicitada");
-  return "row no encontrada";
-}
-char* trash= get_string_from_buffer(auxrow,0);
-int index=strlen(trash);
-char* trash2= get_string_from_buffer(auxrow,index+1);
-int index2=strlen(trash2);
-char* row=get_string_from_buffer(auxrow,index+index2+1);
-}
-return row;*/
+  return row;*/
 }
 
+//TERMINADO
 char* action_insert(package_insert* insert_info){
-  log_info(logger, "Se recibio una accion insert");
 
-  t_config* config = config_create("config"); //consultar equipo si tengo que abrir config acá tambien.
+  //t_config* config = config_create("config"); 
+  //consultar equipo si tengo que abrir config acá tambien.
+  //el punto de montaje es estatico no hace falta
+  //borre todos los mTN point salvo el primeroy lo hice global
   
-  char* MNT_POINT = config_get_string_value(config, "PUNTO_MONTAJE");
   char* table_name = insert_info->table_name;
-  char path_table[300];
-  path_table[0] = '\0';
-  strcat(path_table,MNT_POINT);
-  strcat(path_table,"Tables/");
-  strcat(path_table,table_name);
+  char* table_path = strdup("");  //lo convierto a puntero para no tener problema de tamaño
+  
+  strcat(table_path ,MNT_POINT);
+  strcat(table_path ,"Tables/");
+  strcat(table_path ,table_name);
 
+  log_debug(logger, table_path);//logea para saber que no la estas bardeando
   //verificar que la tabla exista en el fileSystem, en caso q no exista informar y continuar.
-  DIR* dir = opendir(path_table);
+  //conviene tener todos los directorios abiertos para no bloquearse abriendo
+  //nada
+  DIR* dir = opendir(table_path);
   if (dir) {
     closedir(dir);
   } else if (ENOENT == errno) {
@@ -261,42 +243,35 @@ char* action_insert(package_insert* insert_info){
   }
 
   //obtener la metadata asociada a dicha tabla (tabla1) ??Para que necesito la metadata???
-  //strcat(path_table,"Metadata.bin"); //path de metadata
-  //FILE* fp_m = fopen(path_table,"r");
-
-  //TODO verificar si existe en memoria una lista de datos a dumpear, de no existir alocar dicha memoria
-  
-  insert_memtable(insert_info, memtable_p);
+  //si vos no sabes...
+ 
+  insert_to_memtable(insert_info);
 
   //el parametro timestamp es opcional, en caso de que un request no lo provea, se usara el valor actual de epoch
-  //char entry[300];
-  //entry[0] = '\0';
-  //strcat(entry,insert_info->timestamp);
-  //strcat(entry,";");
-  //strcat(entry,insert_info->key);
-  //strcat(entry,";");
-  //strcat(entry,insert_info->value);  //TODO verificar si no hace falta el nombre de la tabla también (en la memtable va a hacer falta creo)
-
+  //EL TIMESTAMP ACA TIENE QUE ESTAR SI O SI!!!!!!
+  
   //TODO insertar en la memoria temporal una nueva entrada que contanga los datos del request
+  //esa es la memtable...
+  //debug y
+  log_debug(logger, "Se inserto el valor en la memtable");
 
-  log_info(logger, "Se inserto el valor en la memtable");
-  char* response = "INSERT ok";
+  //char* response = "INSERT ok"; imaginate que cada vez que 
+  //quieras abrir un programa te aparezca un cartel diciendo "se abrio correctamante"
 
-  return response;
+  return "";
   
 }
 
-void action_create(package_create* create_info){
+char* action_create(package_create* create_info){
   log_info(logger, "Se recibio una accion create");
+  return "";
 }
 
 char* action_describe(package_describe* describe_info){
   log_info(logger, "Se recibio una accion describe");
 
-  t_config* config = config_create("config"); //consultar equipo si tengo que abrir config acá tambien.
-  
+  /*
   //defino el punto de partida para recorrer las tablas
-  char* MNT_POINT = config_get_string_value(config, "PUNTO_MONTAJE");
   char path_table[300];
   path_table[0] = '\0';
   strcat(path_table,MNT_POINT);
@@ -375,33 +350,41 @@ char* action_describe(package_describe* describe_info){
   }
 
   //retornar el contenido de dichos archivos de metadata
-  return buff;
-
+  return "buff";//no podes devolver algo que no sea puntero
+*/
+return "";
 }
 
-void action_drop(package_drop* drop_info){
+char* action_drop(package_drop* drop_info){
   log_info(logger, "Se recibio una accion drop");
+  return "";
 }
 
-void action_journal(package_journal* journal_info){
+char* action_journal(package_journal* journal_info){
   log_info(logger, "Se recibio una accion select");
+  return "";
 }
 
-void action_add(package_add* add_info){
+char* action_add(package_add* add_info){
   log_info(logger, "Se recibio una accion select");
+  return "";
 }
 
 char* action_run(package_run* run_info){
   log_info(logger, "Se recibio una accion run");
 }
 
-void action_metrics(package_metrics* metrics_info){
+char* action_metrics(package_metrics* metrics_info){
   log_info(logger, "Se recibio una accion metrics");
+  return "";
 }
 
+//ACA VA A HABER QUE CREAR THREADS DE EJECUCION
 char* parse_input(char* input){
   return exec_instr(input);
 }
+
+char* action_intern_memory_status(){ return "";};
 
 
 
--- filesystem/memtable.c
+++ filesystem/memtable.c
@@ -4,23 +4,21 @@
 #include <string.h>
 #include <commons/config.h>
 #include "memtable.h"
-
 #include <errno.h>
+#include "../pharser.h"
 
 //struct table_node* memtable_first = NULL;
+//TODO ACA TIENE QUE USAR SEMAFOTOS
+struct table_node* memtable_p;
 
-void insert_memtable(void* args, struct table_node* memtable_p) {
+void insert_to_memtable(package_insert* insert_info) {
 
-    typedef struct {
-        char* table_name;
-        int key;
-        char* value;
-        unsigned timestamp;
-    } insert_info_s;
-
-    insert_info_s* insert_info = args;
+    //Porque tenes copia de los structs???
     
     if (memtable_p == NULL) { //la memtable esta vacia - agrego tabla nueva y cargo la data
+        
+        printf("La mem table eesta vacia\n");
+        
         struct table_node* new_nodo_table;
         new_nodo_table = (struct table_node*)malloc(sizeof(struct table_node));
         new_nodo_table->table_next = NULL;
@@ -40,20 +38,35 @@ void insert_memtable(void* args, struct table_node* memtable_p) {
         //agrego la tabla a la memtable
         memtable_p = new_nodo_table;
 
-        free(new_nodo_table);
-        free(new_nodo_data);
+        /*free(new_nodo_table);
+        free(new_nodo_data);*/ //sensillamente no, es deshacer todo lo que acabas de hacer
 
     } else { //la memtable no esta vacia - busco si existe la tabla
         struct table_node* aux_table;
         aux_table = memtable_p;
-        while(aux_table->table_name != insert_info->table_name && aux_table->table_next != NULL) { //si la tabla en la que estoy parado no es
+            printf("voy a buscar una tabla\n");
+
+        while(aux_table->table_next != NULL) { //si la tabla en la que estoy parado no es
+            printf("encontre una tabla de nombre: %s\n",aux_table->table_name );
+            
+            if(!strcmp(aux_table->table_name,insert_info->table_name)){
+                break;
+            } 
             aux_table = aux_table->table_next;
+
         }
-        if (aux_table->table_name == insert_info->table_name) { //Si encontro la tabla - agrego la data
+            printf("salgo del while: %s\n",aux_table->table_name );
+
+        if (!strcmp(aux_table->table_name, insert_info->table_name)) { //Si encontro la tabla - agrego la data ... asi no se comparan strings
             struct data_node* aux_data;
             aux_data = aux_table->data;
+            printf("con los datos:\n");
+
             while (aux_data->data_next != NULL) { //Avanzo hasta el ultimo nodo de data
-                aux_data = aux_data->data_next;
+                printf("%s\n", aux_data->value);
+                aux_data = aux_data->data_next; //no deberiamos aca descartar los varios  timestamps?
+                
+
             }
             //cargo la data 
             struct data_node* new_nodo_data;
@@ -64,10 +77,9 @@ void insert_memtable(void* args, struct table_node* memtable_p) {
             new_nodo_data->data_next = NULL;
             aux_data->data_next = new_nodo_data;
 
-            free(aux_data);
-            free(new_nodo_data);
 
         } else { //No encontro la tabla pero llego al final de la memtable - agrego la tabla y la data
+        printf("no encontre la tabla, la inserto al final \n");
 
         struct table_node* new_nodo_table;
         new_nodo_table = (struct table_node*)malloc(sizeof(struct table_node));
@@ -88,15 +100,8 @@ void insert_memtable(void* args, struct table_node* memtable_p) {
         //agrego la tabla a la memtable
         aux_table->table_next = new_nodo_table;
 
-        free(new_nodo_table);
-        free(new_nodo_data);
-
         }
-
-        free(aux_table);
     }
-
-    free(insert_info);
 
     return;
 }
