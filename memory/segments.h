a72d4877be4ad9997f375e2fecec7d02b0b187de
maticoto20 <maticoto20@gmail.com>
2019-06-14 16:52:58 -0300

    lru  table wip

--- memory/segments.h
+++ memory/segments.h
@@ -8,13 +8,6 @@
 #define TABLE_NAME_SIZE 3
 
 typedef struct{
-  char* name;
-  char* base;
-  int limit;
-  int pages[3];
-}segment_info;
-
-typedef struct{
   int timestamp;
   int key;
   char value[VALUE_SIZE];
@@ -26,31 +19,44 @@ typedef struct page_info{
   struct page_info* next;
 }page_info_t;
 
-typedef struct segmentt{
+typedef struct segment{
   char* name;
   page_info_t* pages;
-  struct segmentt* next;
+  struct segment* next;
 }segment_t;
 
-typedef struct segment{
-  segment_info data;
-  struct segment *next;
-}segment;
+typedef struct{
+  page_info_t** lru_pages;
+  int current_pages;
+}LRU_TABLE_t;
+
+
+// ---- GLOBAL VARIABLES ----
 
 segment_t* SEGMENT_TABLE;
 page_t* MAIN_MEMORY; 
+LRU_TABLE_t* LRU_TABLE;
 int NUMBER_OF_PAGES;
+
+// --------------------------
+
 
 page_t* create_page(int timestamp, int key, char* value);
 page_info_t* create_page_info();
 segment_t* create_segment(char* table_names);
-page_t* find_page(segment_t* segment, int key);
+page_info_t* find_page_info(segment_t* segment, int key);
+void save_page(segment_t* segment, page_t* page);
+void insert_page(segment_t* segment, page_t* page);
+void save_page_to_memory(segment_t* segment, page_t* page, int dirtybit);
+segment_t* find_segment(char* table_name);
 segment_t* find_or_create_segment(char* table_name);
 int find_free_page();
-void save_page(segment_t* segment, page_t* page);
+void add_segment_to_table(segment_t* segment);
 void add_page_to_segment(segment_t* segment, page_info_t* page_info);
 segment_t* get_last_segment(segment_t* SEGMENT_TABLE);
 page_info_t* get_last_page(page_info_t* page_info);
-void add_segment_to_table(segment_t* segment);
-segment_t* find_segment(char* table_name);
 void print_page(page_t* page);
+int find_page_in_LRU(page_info_t* page);
+void update_LRU(page_info_t* page_info);
+void print_LRU_TABLE();
+LRU_TABLE_t* create_LRU_TABLE();
