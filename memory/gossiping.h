03b10d8366696faeadd9c1a21aceef053f38e2e1
maticoto20 <maticoto20@gmail.com>
2019-07-25 06:26:58 -0300

    asd

--- memory/gossiping.h
+++ memory/gossiping.h
@@ -1,14 +1,31 @@
-#include <stdlib.h>
-#include <commons/log.h>
-#include <pthread.h>
-#include <string.h>
-#include <unistd.h>
-#include "../pharser.h"
-#include "../actions.h"
-#include "../console.h"
+#include "../server.h"
+#include <commons/config.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include "segments.h"
+
+
 
 typedef struct gossip{
   int number;
   struct gossip* next;
   struct gossip* prev;
 }gossip_t;
+
+// ---- GLOBAL VARIABLES ----
+gossip_t* gossip_table;
+// --------------------------
+
+gossip_t* create_node();
+void add_node(gossip_t* list, gossip_t* node);
+void remove_node(gossip_t* list, gossip_t* temp);
+gossip_t* create_gossip_table();
+gossip_t* parse_gossip_buffer(int* buffer);
+int* create_gossip_buffer(gossip_t* list);
+gossip_t* find_node(gossip_t* list, int number);
+gossip_t* compare_lists(gossip_t* list1, gossip_t* list2);
+void gossip();
