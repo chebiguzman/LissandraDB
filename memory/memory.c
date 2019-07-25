33c06cd6f01ca6e5f4d08b914efab52e53b52843
maticoto20 <maticoto20@gmail.com>
2019-07-25 15:04:02 -0300

    hay varias cosas provisorias

--- memory/memory.c
+++ memory/memory.c
@@ -14,13 +14,19 @@
 int main_memory_size;
 
 //punto de entrada para el programa y el kernel
+//para inicializar la memoria por ahora hay que mandar dos parametros, el puerto de la memoria
+//y el puerto del seed
+//ejemplo: ./memoria 5001 5004
 int main(int argc, char const *argv[])
 {
   //set up config  
   config = config_create("config");
   char* LOGPATH = config_get_string_value(config, "LOG_PATH");
+  // ---- CAMBIAR
   // int PORT = config_get_int_value(config, "PORT");
-  int PORT = atoi(argv[2]);
+  int PORT = atoi(argv[1]);
+  char* seed_port = (char*)argv[2];
+
   //set up log
   logger = log_create(LOGPATH, "Memory", 1, LOG_LEVEL_INFO);
   log_info(logger, "El log fue creado con exito\n");
@@ -34,29 +40,29 @@ int main(int argc, char const *argv[])
   int reslt = pthread_create(&tid, NULL, create_server, (void*) serverInfo);
 
   //set up fs client 
-  fs_socket = socket(AF_INET, SOCK_STREAM, 0);
-  char* FS_IP = config_get_string_value(config, "FS_IP");
-  int FS_PORT = config_get_int_value(config, "FS_PORT");
-  printf("%s %d\n", FS_IP, FS_PORT);
-  struct sockaddr_in sock_client;
+  // fs_socket = socket(AF_INET, SOCK_STREAM, 0);
+  // char* FS_IP = config_get_string_value(config, "FS_IP");
+  // int FS_PORT = config_get_int_value(config, "FS_PORT");
+  // printf("%s %d\n", FS_IP, FS_PORT);
+  // struct sockaddr_in sock_client;
   
-  sock_client.sin_family = AF_INET; 
-  sock_client.sin_addr.s_addr = inet_addr(FS_IP); 
-  sock_client.sin_port = htons(FS_PORT);
+  // sock_client.sin_family = AF_INET; 
+  // sock_client.sin_addr.s_addr = inet_addr(FS_IP); 
+  // sock_client.sin_port = htons(FS_PORT);
 
-  int connection_result =  connect(fs_socket, (struct sockaddr*)&sock_client, sizeof(sock_client));
+  // int connection_result =  connect(fs_socket, (struct sockaddr*)&sock_client, sizeof(sock_client));
   
-  if(connection_result < 0){
-    log_error(logger, "No se logro establecer la conexion con el File System");
-     // return 0;
-  }
-  else{
-    char* handshake = malloc(16);
-    write(fs_socket, "MEMORY", strlen("MEMORY"));
-    read(fs_socket, handshake, 4);
-    VALUE_SIZE = atoi(handshake);
-    log_info(logger, "La memory se conecto con fs. El hanshake dio como value size %d", VALUE_SIZE);
-  }
+  // if(connection_result < 0){
+  //   log_error(logger, "No se logro establecer la conexion con el File System");
+  //    // return 0;
+  // }
+  // else{
+  //   char* handshake = malloc(16);
+  //   write(fs_socket, "MEMORY", strlen("MEMORY"));
+  //   read(fs_socket, handshake, 4);
+  //   VALUE_SIZE = atoi(handshake);
+  //   log_info(logger, "La memory se conecto con fs. El hanshake dio como value size %d", VALUE_SIZE);
+  // }
 
   // setup memoria principal
   main_memory_size = config_get_int_value(config, "TAM_MEM");
@@ -84,7 +90,7 @@ int main(int argc, char const *argv[])
   pthread_mutex_init(&lru_table_mutex, NULL);
 
   pthread_t tid_gossiping;
-  pthread_create(&tid_gossiping, NULL, gossip, NULL);
+  pthread_create(&tid_gossiping, NULL, gossip, seed_port);
   
   //inicio lectura por consola
   pthread_t tid_console;
