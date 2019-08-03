48063c74fe9131e4628e734a74f56dad260ce157
Ignacio <pugliese.ignacio@gmail.com>
2019-08-03 09:00:50 -0300

    gossiping no crashea

--- memory/memory.c
+++ memory/memory.c
@@ -270,6 +270,8 @@ char* action_intern__status(){
   char div[2] = { '|', '\0' };
   //printf("algo por aca\n");
   char* gossip_table_buffer = create_gossip_buffer(&GOSSIP_TABLE);
+  if(gossip_table_buffer==NULL) return strdup("");
+
   char* buffer = malloc(strlen(retardo_gossip) + strlen(gossip_table_buffer) + 2);
   *buffer = 0;
   strcpy(buffer, id);
@@ -281,6 +283,7 @@ char* action_intern__status(){
   pthread_mutex_unlock(&gossip_table_mutex);  
 
   //log_error(logger, "%s",buffer);
+  if(buffer==NULL) return strdup("");
   return buffer;
 }
 
